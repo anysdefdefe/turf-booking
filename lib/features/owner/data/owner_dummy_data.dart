6d96ca993b32f5178208e9af19464d79c740b1e4
sunp <sunpreethvishva89@gmail.com>
2026-04-19 07:30:22 +0530

    refactor(owner): purge dead multi-stadium list and legacy dummy data

--- lib/features/owner/data/owner_dummy_data.dart
+++
@@ -1,9 +0,0 @@
-import 'models/owner_model.dart';
-
-// Change isApproved to true to test the approved state
-const dummyOwner = OwnerModel(
-  id: 'owner-001',
-  name: 'Adam',
-  email: 'adam@courtly.com',
-  isApproved: true,
-);
